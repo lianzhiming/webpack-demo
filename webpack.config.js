16c25390236bbd85096ab35219c61b1442574f92
lianzhiming <15101561025@163.com>
2017-03-29 17:58:21 +0800

    学习webpack第二天

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+//点页面应用
+/*
 var htmlWebpackPlugin=require('html-webpack-plugin');//自动载入js插件
 var path=require('path');
 module.exports={
@@ -27,6 +29,50 @@ module.exports={
                 removeComments:true,//删除注释
                 collapseWhitespace:true//删除空格
             }
+        })
+    ]
+}
+*/
+
+//多页面应用
+var htmlWebpackPlugin=require('html-webpack-plugin');//自动载入js插件
+var path=require('path');
+module.exports={
+    entry:{//入口
+        main:'./src/script/main.js',
+        a:'./src/script/a.js',
+        b:'./src/script/b.js',
+        c:'./src/script/c.js'
+    },
+    output:{
+        path:path.join(__dirname, './dist'),
+        filename:'js/[name]-[chunkhash].js',
+        publicPath:'http://cdn.com/'//设置js路径以http://cdn.com/开头
+    },
+    plugins:[
+        new htmlWebpackPlugin({
+            filename:'a.html',//生成的html文件名
+            template:'index.html',
+            inject:false,//设置生成的js在body中还是在head中
+            title:'this is a.html',//设置html的title
+            //chunks:['main','a']//加入main和a
+            excludeChunks:['b','c']
+        }),
+        new htmlWebpackPlugin({
+            filename:'b.html',//生成的html文件名
+            template:'index.html',
+            inject:false,//设置生成的js在body中还是在head中
+            title:'this is b.html',//设置html的title
+            //chunks:['b']
+            excludeChunks:['a','c']
+        }),
+        new htmlWebpackPlugin({
+            filename:'c.html',//生成的html文件名
+            template:'index.html',
+            inject:false,//设置生成的js在body中还是在head中
+            title:'this is c.html',//设置html的title
+            //chunks:['c']
+            excludeChunks:['a','b']
         })
     ]
 }
